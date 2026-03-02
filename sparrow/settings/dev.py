a83016282faf8884879b22c9cc6888aa372f049c
Paul Souders <paul@axoplasm.com>
2026-03-01 16:27:07 -0800

    wagtail starter project

---
+++ sparrow/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-pnblibdzwrvrv_ar4zzab145wfa=9fne7$v6aixf3um4%-s4jn"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
